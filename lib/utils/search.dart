a741ee241974104dcfc4f05c4fdbdd563abe2bed
RomanBlahuta <blahuta.r@ucu.edu.ua>
2024-07-03 10:47:45 +0300

    Minor code style fix.

--- lib/utils/search.dart
+++ lib/utils/search.dart
@@ -7,6 +7,7 @@ abstract class SolutionAlgorithm {
   }
 }
 
+
 class Bfs extends SolutionAlgorithm {
   Bfs();
 
@@ -29,6 +30,7 @@ class Bfs extends SolutionAlgorithm {
     }
   }
 }
+
 
 class ShortestPathSolver {
   SolutionAlgorithm algorithm;
@@ -65,48 +67,3 @@ class ShortestPathSolver {
     return result;
   }
 }
-
-// void bfs(GridGraph graph, Cell source, Map<Cell, Cell?> parents, Map<Cell, int> distances) {
-//   final q = Queue<Cell>();
-//   distances[source] = 0;
-//   q.enqueue(source);
-//
-//   while (!q.empty()) {
-//     final cell = q.dequeue();
-//
-//     for (Cell neighbor in graph.graph[cell]!) {
-//       if (distances[neighbor] == -1) {
-//         parents[neighbor] = cell;
-//         distances[neighbor] = distances[cell]! + 1;
-//         q.enqueue(neighbor);
-//       }
-//     }
-//   }
-// }
-//
-//
-// List<Cell> findShortestPath(GridGraph graph, Cell source, Cell destination) {
-//   final shortestPath = <Cell>[];
-//   final parents = <Cell, Cell?>{};
-//   final distances = <Cell, int>{};
-//   for (final vertice in graph.cells) {
-//     parents[vertice] = null;
-//     distances[vertice] = -1;
-//   }
-//   bfs(graph, source, parents, distances);
-//
-//   if (distances[destination] == -1) {
-//     return shortestPath;
-//   }
-//
-//   Cell currentCell = destination;
-//   shortestPath.add(currentCell);
-//
-//   while (parents[currentCell] != null) {
-//     shortestPath.add(parents[currentCell]!);
-//     currentCell = parents[currentCell]!;
-//   }
-//   final result = shortestPath.reversed.toList();
-//
-//   return result;
-// }
